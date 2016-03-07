7dd5ee5b224bd609ae5380294cabd0a8f8969a7a
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-03-07 18:32:04 +0900

    Finish Component Properties

--- app/comment-list.component.ts
+++ app/comment-list.component.ts
@@ -1,13 +1,16 @@
 import {Component} from 'angular2/core';
+import {CommentComponent} from './comment.component';
 
 @Component({
     selector: 'my-comment-list',
     template: 
     `
     <div className="commentList">
-      Hello, world! I am a CommentList.
+      <my-comment author="Pete Hunt" comment="This is one comment"></my-comment>
+      <my-comment author="Jordan Walke" comment="This is *another* comment"></my-comment>
     </div>
-    `
+    `,
+    directives: [CommentComponent]
 })
 
 export class CommentListComponent { }
