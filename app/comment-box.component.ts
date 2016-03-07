5c51e6ee90531266f8ab8111a46a34d570f99536
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-03-07 17:43:13 +0900

    Finish Your first component

---
+++ app/comment-box.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-comment-box',
+    template: 
+    `
+    <div className="commentBox">
+      Hello, world! I am a CommentBox.
+    </div>
+    `
+})
+
+export class CommentBoxComponent {}
