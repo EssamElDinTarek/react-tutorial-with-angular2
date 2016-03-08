e335fc0a9dfc07dea5ddf1b3a8aaba9720829de4
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-03-08 10:03:39 +0900

    Finish Fetching from the server

--- app/comment-box.component.ts
+++ app/comment-box.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {CommentListComponent} from './comment-list.component';
 import {CommentFormComponent} from './comment-form.component';
+import {CommentService} from './comment.service';
 import {Comment} from './comment'
 
 @Component({
@@ -14,11 +15,15 @@ import {Comment} from './comment'
     </div>
     `,
     directives: [CommentListComponent, CommentFormComponent],
+    providers: [CommentService]
 })
 
 export class CommentBoxComponent {
-  public comments: Comment[] = [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-  ];  
+  comments: Comment[];
+
+  constructor(private _commentService: CommentService) { }
+
+  ngOnInit() {
+    this._commentService.getCommentsObservable().subscribe(comments => this.comments = comments)
+  }
 }
