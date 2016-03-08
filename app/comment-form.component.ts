f58c95b8111630aab6a86f4c25b1c6850a4a330e
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-03-08 10:12:16 +0900

    Finish Adding new comments

--- app/comment-form.component.ts
+++ app/comment-form.component.ts
@@ -4,10 +4,25 @@ import {Component} from 'angular2/core';
     selector: 'my-comment-form',
     template: 
     `
-    <div className="commentForm">
-      Hello, world! I am a CommentForm.
-    </div>
+    <form className="commentForm" (ngSubmit)="handleSubmit()">
+      <input [(ngModel)]="author" placeholder="Your name" />
+      <input [(ngModel)]="text" placeholder="Say something..." />
+      <input type="submit" value="Post" />
+    </form>
     `
 })
 
-export class CommentFormComponent { }
+export class CommentFormComponent { 
+  public author: string
+  public text: string
+  
+  handleSubmit(e) {
+    if (!this.author || !this.text) {
+       return;
+     }
+         
+    // TODO: send request to the server         
+    this.author = ''
+    this.text = ''
+  }  
+}
